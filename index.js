969f1eeb3b41b293fae7b51403b0201caca98393
talhaYZ7 <110539630+talhaYZ7@users.noreply.github.com>
2022-08-03 16:58:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function calcPercentage(number) {
+  // Replace your total marks with 1100.
+  return Math.round((number / 1100) * 100);
+}
+// Replace the marks you obtained with 776.
+console.log(calcPercentage(776));
